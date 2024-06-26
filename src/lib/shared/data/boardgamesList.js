f253f4f00dd8cbf7ae766b895f23cca0d512cf46
DarkDev <darkdev_brand@riseup.net>
2024-06-26 19:55:45 +0300

    add 12262

--- src/lib/shared/data/boardgamesList.js
+++ src/lib/shared/data/boardgamesList.js
@@ -19,13 +19,20 @@ const boardgamesList = [
 		link: '/',
 		anchor: '#heritage-stories',
 		desc: `Heritage stories - educational boardgame, enhanced by an interactive web application, about eco-friendly bicycle trip through the World Heritage Cities.`
-	}, 
+	},
 	{
 		title: 'CASTLE-STORIES',
 		img: '/images/boardgames/castle-stories.svg',
 		link: '/',
 		anchor: '#castle-stories',
 		desc: `Castle stories - educational boardgame, enhanced by an interactive web application, which will take you to the world of grand castles and ancient fortifications! Collect historical facts, complete tasks and unlock the secrets of ancient buildings to become a true connoisseur of medieval architecture.`
+	},
+	{
+		title: '12262-STORIES',
+		img: '/images/boardgames/12262-stories.svg',
+		link: '/',
+		anchor: '#12262-stories',
+		desc: `Explore the Kolskaya ultra-deep borehole, one of the deepest man-made facilities on the planet, drilled to a record depth of 12,262 meters. Discover a symbol of scientific and technical progress that has provided unique data for geological and geophysical research and generated many myths and legends and it's been called "the well to Hell."`
 	}
 ];
 
---
+++ src/lib/shared/data/otherBoardgames.js
@@ -0,0 +1,11 @@
+const boardgamesList = [
+	{
+		title: 'Cultures of Belarus and Poland',
+		img: '/images/boardgames/paleo-stories.svg',
+		link: 'https://by-pl.vercel.app/',
+    desc: `"Cultures of Belarus and Poland" - through the materials prepared as part of this project, you will learn more about the relationship between these two neighboring countries, their history, culture and traditions.`
+  },
+];
+
+
+export default boardgamesList;
