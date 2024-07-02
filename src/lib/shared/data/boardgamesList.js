73d2f1050ce3b1ce3e9b125767dfdbf9978b8f86
DarkDev <darkdev_brand@riseup.net>
2024-07-02 20:41:30 +0300

    upd boardgameslist

--- src/lib/shared/data/boardgamesList.js
+++ src/lib/shared/data/boardgamesList.js
@@ -16,38 +16,38 @@ const boardgamesList = [
 	{
 		title: 'HERITAGE-STORIES',
 		img: '/images/boardgames/heritage-stories.svg',
-		link: '/',
+		link: 'https://heritage-stories.vercel.app/',
 		anchor: '#heritage-stories',
 		desc: `Heritage stories - educational boardgame, enhanced by an interactive web application, about eco-friendly bicycle trip through the World Heritage Cities.`
 	},
 	{
 		title: 'CASTLE-STORIES',
 		img: '/images/boardgames/castle-stories.svg',
-		link: '/',
+		link: 'https://castle-stories.vercel.app/',
 		anchor: '#castle-stories',
 		desc: `Castle stories - educational boardgame, enhanced by an interactive web application, which will take you to the world of grand castles and ancient fortifications! Collect historical facts, complete tasks and unlock the secrets of ancient buildings to become a true connoisseur of medieval architecture.`
 	},
 	{
 		title: '12262-STORIES',
 		img: '/images/boardgames/12262-stories.svg',
-		link: '/',
+		link: 'https://12262-stories.vercel.app/',
 		anchor: '#12262-stories',
 		desc: `Explore the Kolskaya ultra-deep borehole, one of the deepest man-made facilities on the planet, drilled to a record depth of 12,262 meters. Discover a symbol of scientific and technical progress that has provided unique data for geological and geophysical research and generated many myths and legends and it's been called "the well to Hell."`
-  },	{
+	},
+	{
 		title: 'pyramid-stories',
 		img: '/images/boardgames/pyramid-stories.svg',
-		link: '/',
+		link: 'https://pyramid-stories.vercel.app/',
 		anchor: '#pyramid-stories',
-		desc: ``
-  },
-
-
+		desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.`
+	},
 	{
 		title: 'Belarus/Poland-STORIES',
 		img: '/images/boardgames/by-pl-stories.svg',
 		link: 'https://by-pl.vercel.app/',
 		anchor: '#12262-stories',
-		desc: `"Cultures of Belarus and Poland" - through the materials prepared as part of this project, you will learn more about the relationship between these two neighboring countries, their history, culture and traditions.`	}
+		desc: `"Cultures of Belarus and Poland" - through the materials prepared as part of this project, you will learn more about the relationship between these two neighboring countries, their history, culture and traditions.`
+	}
 ];
 
 
