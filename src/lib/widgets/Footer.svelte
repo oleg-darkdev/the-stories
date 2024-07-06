70555505a9ddfd0fb4d41b7b11846b132e585267
DarkDev <darkdev_brand@riseup.net>
2024-07-06 23:28:26 +0300

    clear

--- src/lib/widgets/Footer.svelte
+++ src/lib/widgets/Footer.svelte
@@ -14,7 +14,7 @@
         <div class="footer-grid">
           <div class="footer-grid-left">
             <a href="/" class="w-inline-block w--current">
-              <img src="/images/logo.png" loading="lazy" alt="The-stories logo" />
+              <img src="/images/logo.png" loading="lazy" class='' alt="The-stories logo" />
             </a>
           </div>
           <div class="footer-grid-right">
@@ -24,13 +24,13 @@
                   <h3 class="heading-h6 text-uppercase">Boardgames</h3>
                 </div>
                 <div class="nav"> {#each boardgamesList as boardgame}
-                    
+
 									<a href="{boardgame.link}" target="_blank"  class="nav-item inverse w-inline-block w--current">
 										<div class="nav-item-text">{boardgame.title}</div>
 										<div class="nav-item-line inverse"></div>
 									</a>
                   {/each}
-                    
+
 									<div class="w-[180px] -mt-4 margin-small">
 										<a href="https://oleg-darkdev.vercel.app/" target="_blank" class="button secondary w-inline-block">
 											<div class="button-inner">
