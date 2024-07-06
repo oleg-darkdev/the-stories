152bd413343da34d1582b3ab604e0f77ab815d92
DarkDev <darkdev_brand@riseup.net>
2024-07-06 09:29:27 +0300

    upd text

--- src/lib/shared/data/boardgamesList.js
+++ src/lib/shared/data/boardgamesList.js
@@ -2,35 +2,36 @@ const boardgamesList = [
 	{
 		title: 'PALEO-STORIES',
 		img: '/images/boardgames/paleo-stories.svg',
-		link: '/',
+		link: 'https://paleo-stories.vercel.app/',
 		anchor: '#paleo-stories',
-		desc: `Paleo Stories - educational boardgame, enhanced by the interactive web application, that introduces players to the world of paleontological expeditions and the science of paleontology. `
+		desc: `Educational boardgame, enhanced by the interactive web application, that introduces players to the world of paleontological expeditions and the science of paleontology. `
 	},
 	{
 		title: 'ARCHAEO-STORIES',
 		img: '/images/boardgames/archaeo-stories.svg',
 		link: 'https://archaeo-stories.vercel.app/',
 		anchor: '#archaeo-stories',
-		desc: `Archaeo stories - educational boardgame, enhanced by an interactive web application, offers you to explore ancient grounds, unearth artifacts, and  become a real archaeologist! `
+		desc: `Educational boardgame, enhanced by an interactive web application, offers you to explore ancient grounds, unearth artifacts, and  become a real archaeologist! `
 	},
 	{
 		title: 'HERITAGE-STORIES',
 		img: '/images/boardgames/heritage-stories.svg',
 		link: 'https://heritage-stories.vercel.app/',
 		anchor: '#heritage-stories',
-		desc: `Heritage stories - educational boardgame, enhanced by an interactive web application, about eco-friendly bicycle trip through the World Heritage Cities.`
+		desc: `Educational boardgame, enhanced by an interactive web application, about eco-friendly bicycle trip through the World Heritage Cities.`
 	},
 	{
 		title: 'CASTLE-STORIES',
 		img: '/images/boardgames/castle-stories.svg',
 		link: 'https://castle-stories.vercel.app/',
 		anchor: '#castle-stories',
-		desc: `Castle stories - educational boardgame, enhanced by an interactive web application, which will take you to the world of grand castles and ancient fortifications! Collect historical facts, complete tasks and unlock the secrets of ancient buildings to become a true connoisseur of medieval architecture.`
+		desc: `Educational boardgame, enhanced by an interactive web application, which will take you to the world of grand castles and ancient fortifications! Collect historical facts, complete tasks and unlock the secrets of ancient buildings to become a true connoisseur of medieval architecture.`
 	},
 	{
 		title: '12262-STORIES',
 		img: '/images/boardgames/12262-stories.svg',
-		link: 'https://12262-stories.vercel.app/',
+		// https://12262-stories.vercel.app/
+		link: '/',
 		anchor: '#12262-stories',
 		desc: `Explore the Kolskaya ultra-deep borehole, one of the deepest man-made facilities on the planet, drilled to a record depth of 12,262 meters. Discover a symbol of scientific and technical progress that has provided unique data for geological and geophysical research and generated many myths and legends and it's been called "the well to Hell."`
 	},
@@ -39,8 +40,8 @@ const boardgamesList = [
 		img: '/images/boardgames/pyramid-stories.svg',
 		link: 'https://pyramid-stories.vercel.app/',
 		anchor: '#pyramid-stories',
-		desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.`
-	},
+		desc: `Educational board game, enhanced by an interactive web application, where players conduct archaeological excavations of old pyramids in search of ancient artifacts. In the game process, players learn about history, culture using advanced technologies.`
+	}
 	// {
 	// 	title: 'Belarus/Poland-STORIES',
 	// 	img: '/images/boardgames/by-pl-stories.svg',
